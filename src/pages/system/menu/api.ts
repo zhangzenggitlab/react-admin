454457d4fa86a67aabc3844eb129c42de8c2d657
zz <zz@heshuruanjian.cn>
2024-02-28 10:27:08 +0800

    解决文件冲突问题

--- src/pages/system/menu/api.ts
+++ src/pages/system/menu/api.ts
@@ -10,11 +10,7 @@ export const getMenu = () => {
                     icon: "***",
                     sort: 1,
                     permission: "menu:*",
-<<<<<<< HEAD
                     type:1,
-=======
-                    type: "1",
->>>>>>> fbe1d6275972365c94fb0aa669deca84ad170a6a
                     status: 1,
                     menu: "system",
                     createTime: 1534837621348,
@@ -26,13 +22,8 @@ export const getMenu = () => {
                         icon: "***",
                         sort: 1,
                         permission: "menu:user:list",
-<<<<<<< HEAD
                         type: 2,
                         status:2,
-=======
-                        type: "2",
-                        status: 2,
->>>>>>> fbe1d6275972365c94fb0aa669deca84ad170a6a
                         menu: "system",
                         createTime: 1534837621348,
                         parentId: 1
