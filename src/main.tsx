9c39fd338450b45dcfe89e16e0a80db994a4b610
zhangzeng <zeng.zhang@ysinc.com>
2024-10-18 18:30:33 +0800

    feat

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 
 import './index.css'
 import { router } from '@/router'
+import {BaseRouter} from '@/lib'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BaseRouter router={router} />
   </StrictMode>,
 )
