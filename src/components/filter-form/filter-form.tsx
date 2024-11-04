7c476270c6d6ebf2389cff9e221b6fc9e5013693
zhangzeng <zeng.zhang@ysinc.com>
2024-11-04 15:16:13 +0800

    feat:filter-form组件

--- src/components/filter-form/filter-form.tsx
+++ src/components/filter-form/filter-form.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Form, FormProps as AntFormProps } from 'antd'
 import clsx from 'clsx'
 import { css } from '@emotion/react'
+import { Form, FormProps as AntFormProps } from 'antd'
 
 import { Button } from '@/components'
 
@@ -11,10 +11,14 @@ export interface FormProps extends AntFormProps {
   onSearch?: () => void
 }
 
+const DEFINE = {
+  rowHeight:32,
+}
+
 export const FilterForm = (props: FormProps) => {
   const [collapsed, setCollapsed] = React.useState<boolean>(false)
 
-  return <div className={clsx(formClass, 'flex start-x ovh p-x-24 p-y-4 width')}>
+  return <div className={clsx(formClass, 'flex start-x ovh p-x-24 p-y-4 width ovh')} style={{height: collapsed ? DEFINE.rowHeight+'px':'auto'}}>
     <Form {...props} className="filter-form flex gap-24 wrap" >{props.children}</Form>
 
     <div className="flex gap-10">
